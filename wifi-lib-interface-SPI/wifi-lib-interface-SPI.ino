0a97529bc07312cc4babe9f028fa2110f60dd8f9
andrea-83 <andrea@arduino.org>
2017-01-31 14:55:15 +0100

    added UI

--- wifi-lib-interface-SPI/wifi-lib-interface-SPI.ino
+++ wifi-lib-interface-SPI/wifi-lib-interface-SPI.ino
@@ -1,30 +1,34 @@
 #include "CommLgc.h"
-#include "utility/wifi_utils.h"
+//#include "utility/wifi_utils.h"
 
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 
-// char ssid[] = "****";     //  your network SSID (name)
-// char pass[] = "****";
+#include <FS.h>
+#include <Hash.h>
+#include <ESP8266WebServer.h>
+
+#define WIFI_LED 2
+#define HOSTNAME "Arduino-PrimoSPI"
+
+ESP8266WebServer server(80);    //server UI
 
 void setup() {
 
-  // WiFi.mode(WIFI_STA);
-  // WiFi.begin(ssid, pass);
-  // while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-  //   delay(5000);
-  //   ESP.restart();
-  // }
-	// delay(8000);
   ArduinoOTA.begin();
   CommunicationLogic.begin();
+  WiFi.hostname(HOSTNAME);      //set hostname
+  MDNS.begin(HOSTNAME);         //set mdns
+  initWBServer();               //UI begin
 
 }
 
 void loop() {
+  
   ArduinoOTA.handle();
   CommunicationLogic.handle();
+  handleWBServer();
 
 }
