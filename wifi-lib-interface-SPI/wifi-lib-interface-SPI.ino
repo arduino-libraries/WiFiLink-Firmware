efb4ed6a94dd0de249bd14db69fe9c55bd61b8a6
andrea-83 <andrea@arduino.org>
2017-03-01 16:12:51 +0100

    added ui check and mdns begin

--- wifi-lib-interface-SPI/wifi-lib-interface-SPI.ino
+++ wifi-lib-interface-SPI/wifi-lib-interface-SPI.ino
@@ -1,3 +1,4 @@
+
 #include "CommLgc.h"
 //#include "utility/wifi_utils.h"
 
@@ -10,25 +11,43 @@
 #include <Hash.h>
 #include <ESP8266WebServer.h>
 
-#define WIFI_LED 2
-#define HOSTNAME "Arduino-PrimoSPI"
+#define HOSTNAME "Arduino-Primo"
 
 ESP8266WebServer server(80);    //server UI
-
+bool SERVER_STOP = false;       //check stop server
+// MCU McuType = NRF52;
 void setup() {
 
   ArduinoOTA.begin();
   CommunicationLogic.begin();
-  WiFi.hostname(HOSTNAME);      //set hostname
-  MDNS.begin(HOSTNAME);         //set mdns
   initWBServer();               //UI begin
+  initMDNS();                   //set MDNS
+  //Serial1.println("end setup");
 
 }
 
 void loop() {
-  
+
   ArduinoOTA.handle();
   CommunicationLogic.handle();
-  handleWBServer();
+  if(CommunicationLogic.UI_alert){			//stop UI SERVER){}
+    if(!SERVER_STOP){
+      server.stop();
+      SERVER_STOP = true;
+    }
+  }
+  else
+    handleWBServer();
+  // unsigned long currentMillis = millis();
+  // if (currentMillis - previousMillis >= interval) {
+  //   // save the last time you blinked the LED
+  //   previousMillis = currentMillis;
+  //   if (ledState == LOW) {
+  //     ledState = HIGH;
+  //   } else {
+  //     ledState = LOW;
+  //   }
+  //   digitalWrite(WIFI_LED, ledState);
+  // }
 
 }
