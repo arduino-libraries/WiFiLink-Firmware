ef52b3a591e555b118a5074dfc3763de77dc1605
sergiotomasello <sergiotomasello@users.noreply.github.com>
2017-03-02 11:01:19 +0100

    upd: set new defines in config.h

--- wifi-lib-interface-SPI/config.h
+++ wifi-lib-interface-SPI/config.h
@@ -1,14 +1,34 @@
 
+/*
+ * Define board model name
+ *
+ */
 
+#define BOARDMODEL "STARTOTTO"
+//#define BOARDMODEL "PRIMO"
+//#define BOARDMODEL "UNOWFI"
 
-#define ESP_CH_SPI
-#define BAUDRATE_DEBUG 115200
-#define WIFI_LED 2
-#define DEBUG
+/*
+ * Defines the communication channel between microcontroller
+ * and esp82266, with concerning parameters
+ *
+ */
 
-
-// #define ESP_CH_UART
-// #define BAUDRATE_COMMUNICATION 115200
-// #define BAUDRATE_DEBUG 115200
-// #define DEBUG
-// #define WIFI_LED 14
+if (BOARDMODEL == "STARTOTTO"){
+  //Arduino STAR OTTO configuration parameters
+  #define ESP_CH_UART
+  #define BAUDRATE_COMMUNICATION 460800
+  #define BAUDRATE_DEBUG 115200
+  #define DEBUG
+  #define WIFI_LED 14
+  }
+else if(BOARDMODEL == "PRIMO"){
+  //Arduino PRIMO configuration parameters
+  #define ESP_CH_SPI
+  #define BAUDRATE_DEBUG 115200
+  #define WIFI_LED 2
+  #define DEBUG
+  }
+else if{
+  //Arduino UNO WIFI configuration parameters
+}
