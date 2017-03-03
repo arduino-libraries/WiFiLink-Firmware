e81ce9a3c1bf8610597f811c17c84d9816d81d0e
andrea-83 <andrea@arduino.org>
2017-03-03 10:20:23 +0100

    fix: remove hostname define

--- ArduinoFirmwareEsp/ArduinoFirmwareEsp.ino
+++ ArduinoFirmwareEsp/ArduinoFirmwareEsp.ino
@@ -1,5 +1,4 @@
 #include "CommLgc.h"
-//#include "utility/wifi_utils.h"
 
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
@@ -11,11 +10,9 @@
 #include <Hash.h>
 #include <ESP8266WebServer.h>
 
-#define HOSTNAME "Arduino-Primo"
-
 ESP8266WebServer server(80);    //server UI
 bool SERVER_STOP = false;       //check stop server
-// MCU McuType = NRF52;
+
 void setup() {
 
   ArduinoOTA.begin();
@@ -29,7 +26,7 @@ void loop() {
 
   ArduinoOTA.handle();
   CommunicationLogic.handle();
-  if(CommunicationLogic.UI_alert){			//stop UI SERVER){}
+  if(CommunicationLogic.UI_alert){			//stop UI SERVER
     if(!SERVER_STOP){
       server.stop();
       SERVER_STOP = true;
@@ -37,16 +34,5 @@ void loop() {
   }
   else
     handleWBServer();
-  // unsigned long currentMillis = millis();
-  // if (currentMillis - previousMillis >= interval) {
-  //   // save the last time you blinked the LED
-  //   previousMillis = currentMillis;
-  //   if (ledState == LOW) {
-  //     ledState = HIGH;
-  //   } else {
-  //     ledState = LOW;
-  //   }
-  //   digitalWrite(WIFI_LED, ledState);
-  // }
 
 }
