9b82e1c141f16abc77501a20bce3c167fba0fba5
andrea-83 <andrea@arduino.org>
2017-03-13 18:57:44 +0100

    fix: added Serial communication release

--- ArduinoFirmwareEsp/ArduinoMcuOTA.ino
+++ ArduinoFirmwareEsp/ArduinoMcuOTA.ino
@@ -18,11 +18,20 @@
 struct dfu_data *global_dfu;
 struct dfu_binary_file *global_binary_file;
 
+static int serial_release(void *dummy)
+{
+  //stop Serial communication
+  Serial.end();
+  return 0;
+}
+
 static int _setup_dfu(void)
 {
   #if defined(STAROTTO)
   global_dfu = dfu_init(&esp8266_serial_star8_interface_ops,
                 NULL,
+                NULL,
+                serial_release,
                 NULL,
                 &stm32_dfu_target_ops,
                 &stm32f469bi_device_data,
@@ -31,32 +40,25 @@ static int _setup_dfu(void)
   global_dfu = dfu_init(&esp8266_serial_arduino_unowifi_interface_ops,
                 NULL,
                 NULL,
+                serial_release,
+                NULL,
                 &stk500_dfu_target_ops,
                 &atmega328p_device_data,
                 &esp8266_dfu_host_ops);
   #endif
 
   if (!global_dfu) {
-    #if defined(DEBUG)
-    Serial1.printf("Error initializing dfu library");
-    #endif
     /* FIXME: Is this ok ? */
     return -1;
   }
 
-  global_binary_file = dfu_binary_file_start_rx(&dfu_rx_method_http_arduino, global_dfu, NULL);
+  global_binary_file = dfu_binary_file_start_rx(&dfu_rx_method_http_arduino, global_dfu, &server);
   if (!global_binary_file) {
-    #if defined(DEBUG)
-    Serial1.printf("Error instantiating binary file");
-    #endif
     return -1;
   }
 
   if (dfu_binary_file_flush_start(global_binary_file) < 0) {
-    #if defined(DEBUG)
-    Serial1.printf("Error in dfu_binary_file_flush_start()");
-    #endif
-    return -1;
+      return -1;
   }
   return 0;
 }
@@ -71,27 +73,24 @@ void _finalize_dfu(void)
 
 void _handle_Mcu_OTA(void)
 {
-  if (!global_dfu)
-    _setup_dfu();
-  if (!global_dfu)
-    return;
-  switch (dfu_idle(global_dfu)){
+   if (!global_dfu)
+     _setup_dfu();
+   if (!global_dfu)
+     return;
+   switch (dfu_idle(global_dfu)) {
     case DFU_ERROR:
-      #if defined(DEBUG)
-      Serial1.printf("Error programming file");
-      #endif
       _finalize_dfu();
       break;
     case DFU_ALL_DONE:
-      #if defined(DEBUG)
-      Serial1.printf("Programming OK");
-      #endif
       dfu_target_go(global_dfu);
       _finalize_dfu();
+      delay(80);
+      //open Serial Communication
+      Serial.begin(BAUDRATE_COMMUNICATION);
       break;
     case DFU_CONTINUE:
       break;
-  }
+    }
 }
 
 #endif
